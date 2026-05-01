a7cc1d1639557193b67c5276bef03ed2e3437082
M. Abdul-Rasheed Talal <mabdulrasheedtalal@gmail.com>
2026-05-01 16:46:50 +0500

    Add YouTube Manager app with MongoDB integration for video management

---
+++ Projects/03_youtube_manager_with_mongoDB.py/youtube_manager.py
@@ -0,0 +1,54 @@
+from pymongo import MongoClient
+from bson import ObjectId
+client = MongoClient("mongodb://localhost:27017/ytmanager")
+
+db = client["ytmanager"]
+video_collection = db["videos"]
+
+# print(video_collection)
+
+def add_video(name , time):
+    video_collection.insert_one({"name":name , "time":time})
+
+def list_videos():
+    for video in video_collection.find():
+        print(f"Name : {video['name']}\nTime : {video['time']}")
+
+def update_video(video_id , new_name , new_time):
+    video_collection.update_one(
+        {'_id': ObjectId (video_id)} ,
+        {"$set": {"name" : new_name , "time":new_time}})
+
+def delete_video(video_id):
+    video_collection.delete_one({'_id': ObjectId (video_id)})
+
+
+def main():
+    while True:
+        print("\n Youtube Manager App")
+        print("1. List all videos")
+        print("2. Add new videos")
+        print("3. Update videos")
+        print("4. Delete videos")
+        print("5. Exit App")
+        choice = input("Enter your choice : ")
+
+        if choice == '1':
+            list_videos()
+        elif choice == '2':
+            name = input("Enter the video name : ")
+            time = input("Enter the video time  : ")
+            add_video(name , time)
+        elif choice == '3':
+            video_id = input("Enter the video_id to update : ")
+            name = input("Enter the updated video name : ")
+            time = input("Enter the updated video time  : ")
+            update_video(video_id , name , time)
+        elif choice == '4':
+            video_id = input("Enter the video_id to delete : ")
+            delete_video(video_id)
+        else:
+            print("Invalid Choice !")
+
+if __name__ == "__main__":
+    main()
